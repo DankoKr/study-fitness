915a5f55a52f37ec98fdcbc92de62f970a32e004
Danko Kralski <d.kralski@student.fontys.nl>
2023-03-02 13:41:58 +0100

    Adding BMI, TDEE and Login page, UML upgrades, new UserAdministration class, improved code for Form

--- Study Fitness (Website)/Study Fitness/Pages/StudyFitness pages/BMI.cshtml.cs
+++ Study Fitness (Website)/Study Fitness/Pages/StudyFitness pages/BMI.cshtml.cs
@@ -1,35 +1,33 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using Study_Fitness.Models;
 
 namespace Study_Fitness.Pages
 {
     public class BMIModel : PageModel
     {
-        public bool hasData = false;
-        public string? sex;
-        public int age;
-        public double height;
-        public double weight;
-        public double result;
+        [BindProperty]
+        public BMICalculator? calculator { get; set; }
+
+        public String? Message { get; set; }
 
         public void OnGet()
         {
         }
 
-        public void OnPost() 
+        public IActionResult OnPost()
         {
-            hasData = true;
-            sex = Request.Form["sex"];
-            age = Convert.ToInt32(Request.Form["age"]);
-            height = Convert.ToDouble(Request.Form["height"]);
-            weight = Convert.ToDouble(Request.Form["weight"]);
-            Calculate(height, weight);
+            if (ModelState.IsValid)
+            {
+                if (calculator != null)
+                {
+                    calculator.Calculate();
+                    Message = "You are a " + calculator.Sex + " (age: "+ calculator.Age + ")" + " with BMI: " + calculator.myBMI;                    
+                }
+            }
+            return Page();
+
         }
 
-        public void Calculate(double height, double weight) 
-        {
-            result = weight / height * height;
-        }
     }
 }
---
+++ Study_Fitness(App)/Study_Fitness/UserAdministration.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study_Fitness
+{
+    public class UserAdministration
+    {
+        List<User> myUsers = new List<User>();
+
+        public void AddUser(User user) 
+        {
+            myUsers.Add(user);
+        }
+
+        public void DeleteUser(User user) 
+        {
+            myUsers.Remove(user);
+        }
+
+        public void ChangePassword() 
+        {
+        
+        }
+
+    }
+}
